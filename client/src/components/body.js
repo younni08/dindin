dec7841b8fd7fb6c3ead569502aaf039ffd3f151
younni08 <younni08@gmail.com>
2021-03-29 11:21:26 +0900

    check

--- client/src/components/body.js
+++ client/src/components/body.js
@@ -1,20 +1,64 @@
-import react from "react";
+import react,{useState} from "react";
 
 const Body = () => {
+    const [weapon,setWeapon] = useState(false)
+    const [cloth1,setCloth1] = useState(false)
+    const [cloth2,setCloth2] = useState(false)
+    const [cape,setCape] = useState(false)
+    const [shoes,setShoes] = useState(false)
+    const [hat,setHat] = useState(false)
+
     const getDress = () => {
         console.log("aas")
+        setHat(true)
+        setShoes(true)
+        setCape(true)
+        setCloth1(true)
+        setCloth2(true)
+        setWeapon(true)
     }
+
 
 
     return (
         <div className="body">
             <div className="body_level1">
                 <div className="showroom">
-                    <img src="./pics/manikin.png" alt="manikin" />
+                    <img src="./pics/manikin.png" className="manikin" alt="manikin" />
+                    {
+                        hat?<img src="./pics/17hat.png" alt="hat" className="hat" />:""
+                    }
+                    {
+                        cloth1?<img src="./pics/17cloth1.png" alt="cloth" className="cloth" />:""
+                    }
+                    {
+                        weapon?<img src="./pics/17weapon.png" alt="weapon" className="weapon" />:""
+                    }
+                    {
+                        cape?<img src="./pics/17cape.png" alt="cape" className="cape" />:""
+                    }
+                    {
+                        shoes?<img src="./pics/17shoes.png" alt="shoes" className="shoes" />:""
+                    }
                     <span>남자 코디</span>
                 </div>
                 <div className="showroom">
-                    <img src="./pics/manikin.png" alt="manikin" />
+                    <img src="./pics/manikin.png" className="manikin" alt="manikin" />
+                    {
+                        hat?<img src="./pics/17hat.png" alt="hat" className="hat" />:""
+                    }
+                    {
+                        cloth1?<img src="./pics/17cloth2.png" alt="cloth" className="cloth" />:""
+                    }
+                    {
+                        weapon?<img src="./pics/17weapon.png" alt="weapon" className="weapon" />:""
+                    }
+                    {
+                        cape?<img src="./pics/17cape.png" alt="cape" className="cape" />:""
+                    }
+                    {
+                        shoes?<img src="./pics/17shoes.png" alt="shoes" className="shoes" />:""
+                    }
                     <span>여자 코디</span>
                 </div>
             </div>
